4e8ce1864a5228798b7ac1105f565e04717667cb
danieltangdx <danieltangdx@outlook.com>
2020-03-07 07:33:09 +0800

    change the Makefile and run.tcl for counter test

--- counter/run.tcl
+++ counter/run.tcl
@@ -1,8 +1,8 @@
 debug .
 host .
 xc on -xt0 -zt0 -run
-probe -create -add vid_tb_top -depth all
-database -open 818
+probe -create -add counter_tb -depth all
+database -open counter
 xeset traceMemSize 10000000ns
 run 1000000ns
 database -upload
